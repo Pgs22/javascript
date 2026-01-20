b21cf446fe347b7afed08ae12ea0efec2a26bcf8
patricia <patricia.gsbcn@gmail.com>
2026-01-20 19:20:24 +0100

    objetos retproductor

--- practica4/Ex2/reproductor.js
+++ practica4/Ex2/reproductor.js
@@ -1,26 +1,28 @@
 /*Reproductor.html 
 2. Millora el reproductor d’àudio creant les classes Musica i Llista músiques. Segueix els següents punts:  
-    a. Crea una classe Musica amb com a mínim: 
-        i. Getters i setters: 
-            1.  Titol - mínim de dos caràcters 
-        2. nom de l’arxiu - mínim de un caràcter i acabat amb alguna extensió correcte 
-            a. automàticament ha d’assignar la seva extensió com a  “media type”  
-        3. media type  - rebrà la extensió i haurà d’assignar el media type segons: 
-            a. mp3 -> audio/mpeg 
-            b. ogg -> audio/ogg 
-            c. wav -> audio/wav 
-        4. etiquetes – un array de strings per classificar la música (jazz, pop, animades..) 
+    
+    {{ ARXIU musica }} 
+    a. Crea una classe Musica
+
+    {{ ARXIU llistaMusiques }} 
     b. Crea una classe  LlistaMusiques 
-        i. Getters i setters: 
-            1. Titol 
-            2. Etiquetes 
-            3. Llistat de músiques   
+    
+    {{ ARXIU reproductor.html y reproductor.js }}    
     c. Crea un objecte del tipus LlistaMusiques amb: 
         i. Getters i setters: 
             1. Titol: “disponibles” 
             2. Etiquetes “tots” 
             3. Llistat de músiques: ha de contenir la informació de totes les músiques 
             disponibles 
+*/
+import {Musica} from "./Musicajs"
+const etiquetes_disponibles=["jazz", "pop", "animades", "rock", "clasica"];
+const etiquetes_tots=["tots"]
+
+const novaMusica = new Musica("disponibles", "musica", etiquetes_tots)
+console.log(novaMusica.titol);
+
+/*
     d. Permet mostrar la informació d’un àudio 
     e. Permet que l’usuari pugui reproduir, aturar, posar en pausa i pujar i baixar el volum de  
     qualsevol àudio de l’array. 
@@ -34,17 +36,8 @@
 
 */
 
-/*
-    c. Crea un objecte del tipus LlistaMusiques amb: 
-        i. Getters i setters: 
-            1. Titol: “disponibles” 
-            2. Etiquetes “tots” 
-            3. Llistat de músiques: ha de contenir la informació de totes les músiques 
-            disponibles 
-*/
 
-const etiquetes_disponibles=["jazz", "pop", "animades", "rock", "clasica"];
-const etiquetes_tots=["tots"]
+
 
 //aqui creo un objecte de llistamusques
 //aqui creo un objecte de musica
