316df1d3ecda2b6055c73da74d80f52ed8d2b38b
patricia <patricia.gsbcn@gmail.com>
2026-02-19 18:58:37 +0100

    ejemplos p5

---
+++ practica5/activitat5.py
@@ -0,0 +1,12 @@
+# En aquesta activitat heu d’ampliar l’activitat anterior a partir dels següents requisits:
+# Alarma.html
+# 1. Alarma.html ha de tenir un formulari per crear alarmes noves 
+# validant totes les dades abans de crear l’objecte Alarma:
+
+# a. El formulari ha de permetre introduir:
+    # i. Títol de l’alarma (input text)
+    # ii. Hora, minut i segon (inputs number)
+    # iii. Música (select amb les músiques disponibles del reproductor)
+    # iv. Activa (checkbox)
+
+
