55fad9aa390f6859f64bdbd889ae0124be7096f4
patricia <patricia.gsbcn@gmail.com>
2026-05-08 08:42:28 +0200

    actualizado html

--- examen/examen.js
+++ examen/examen.js
@@ -1,39 +1,22 @@
-urlBase = "http://localhost/javascript/examen/servidor.php";
-document.getElementById("btn_crear_nau").addEventListener("click", function () {
+const urlBase = "http://localhost/javascript/examen/servidor.php";
+// codigo 
+document.getElementById("btn_crea_crew").addEventListener("click", function (){
+    let name = document.getElementById("inp_crew_name").value;
+    let age = document.getElementById("inp_crew_age").value;
+    let race = document.getElementById("inp_crew_race").value;
 
-  let xhr = new XMLHttpRequest();
-
-  let name = document.getElementById("inp_name_ship").value;
-  let universe = document.getElementById("inp_universe").value;
-  let armament = document.getElementById("inp_armament").value;
-
-  xhr.open("POST", urlBase, true);
-
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-
-    if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 201)) {
-        console.log("Respuesta del servidor:", xhr.responseText);
-        const response = JSON.parse(xhr.responseText);
-
-        let html = "";
-        html += "<h3>Nau creada</h3>";
-        html += "<p>ID: " + response.data.id + "</p>";
-        html += "<p>Nom: " + response.data.name + "</p>";
-        html += "<p>Univers: " + response.data.universe + "</p>";
-        html += "<p>Armament: " + response.data.armament + "</p>";
-
-        document.getElementById("div_output").innerHTML = html;
-    }
-  };
-
-  let parametros =
-      "action=ship_create" +
-      "&name=" + name +
-      "&universe=" + universe +
-      "&armament=" + armament;
-
-  xhr.send(parametros);
-
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", urlBase, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 201)){
+            response = JSON.parse(xhr.responseText);
+            let html = "";
+            html += response.message;
+            document.getElementById("div_output").innerHTML = html;
+        }
+    };
+    console.log("Enviando petición..");
+    let parametros = "action=crew_create" + "&name=" + name + "&age=" + age + "&race=" + race;
+    xhr.send(parametros);
 });
